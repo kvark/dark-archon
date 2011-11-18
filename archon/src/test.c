ad460f82e3bdbdbae93df5913183fe3d179bed7e
kvark <kvark@localhost>
2011-11-18 11:15:26 -0500

    IT-2x estimation implemented. Produces a lot of small groups, almost twice as effective as IT-2 on random data.

--- archon/src/test.c
+++ archon/src/test.c
@@ -3,7 +3,9 @@
  */ 
 
 #include "common.h"
+#include <malloc.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 
@@ -16,28 +18,118 @@ struct Estimation	{
 	char name[10];
 };
 
+void sort(struct Estimation *const e, int num)	{
+	e->n_flat += num;
+	e->n_log += num * log2(num);
+}
+
 FILE *fi = NULL;
 
 typedef void (*f_estimate)(struct Estimation *const);
 
 
 void fit1(struct Estimation *const e)	{
-	int i, r[256] = {0};
+	int i, r[1<<8] = {0};
 	byte a=0,b=0;
 	fread(&a,1,1,fi);
 	while( fread(&b,1,1,fi) )	{
-		r[a] += (b>a);
+		r[a] += (b>=a);
 		a = b;
 	}
-	for(i=0; i<256; ++i)	{
-		e->n_flat += r[i];
-		e->n_log += r[i] * log2(r[i]);
+	for(i=0; i<(1<<8); ++i)	{
+		sort(e,r[i]);
 	}
 	strcpy(e->name,"IT-1");
 }
 
 void fit2(struct Estimation *const e)	{
+	int i, r[1<<16] = {0};
+	byte a=0,b=0,c=0;
+	fread(&a,1,1,fi);
+	fread(&b,1,1,fi);
+	while( fread(&c,1,1,fi) )	{
+		r[(a<<8)+b] += (c>b && a>=b);
+		a = b; b = c;
+	}
+	for(i=0; i<(1<<16); ++i)	{
+		sort(e,r[i]);
+	}
 	strcpy(e->name,"IT-2");
+}
+
+void fit1x(struct Estimation *const e)	{
+	int i, ra[1<<8] = {0}, rb[1<<16] = {0};
+	byte a=0,b=0;
+	fread(&a,1,1,fi);
+	while( fread(&b,1,1,fi) )	{
+		ra[a] += 1;
+		rb[(a<<8)+b] += 1;
+		a = b;
+	}
+	for(;;)	{
+		int min = 0;
+		for(i=1; i<(1<<8); ++i)	{
+			if(!ra[min] || (ra[i] > 0 && ra[i] < ra[min]))
+				min = i;
+		}
+		if(!ra[min])
+			break;
+		ra[min] = 0;
+		for(i=0; i<(1<<8); ++i)	{
+			int *const x = rb + (min<<8)+i;
+			sort(e,*x);
+			*x = 0;
+		}
+		for(i=0; i<(1<<8); ++i)	{
+			int *const x = rb + (i<<8)+min;
+			if(ra[i]>0)
+				ra[i] -= *x;
+			*x = 0;
+		}
+	}
+	strcpy(e->name,"IT-1x");
+}
+
+static int *r_cmp = NULL;
+int cmp_word(const void *pa, const void *pb)	{
+	return r_cmp[*(word*)pa] - r_cmp[*(word*)pb];
+}
+
+void fit2x(struct Estimation *const e)	{
+	int i, min=0, r[1<<16] = {0};
+	byte a=0,b=0,c=0;
+	int *const rx = malloc(sizeof(int)<<24);
+	word suf[1<<16];
+
+	memset(rx,0,sizeof(int)<<24);
+	fread(&a,1,1,fi);
+	fread(&b,1,1,fi);
+	while( fread(&c,1,1,fi) )	{
+		r[(a<<8)+b] += 1;
+		rx[(a<<16)+(b<<8)+c] += 1;
+		a = b; b = c;
+	}
+	r_cmp = r;
+	for(i=0; i<(1<<16); ++i)
+		suf[i] = i;
+	for(;;++min)	{
+		word s = 0;
+		qsort(suf+min, (1<<16)-min, sizeof(word), cmp_word);
+		while( min<(1<<16) && r[suf[min]]<=0 )
+			++min;
+		if(min == (1<<16))
+			break;
+		s = suf[min];
+		sort(e,r[s]);
+		r[s] = 0;
+		for(i=0; i<(1<<8); ++i)	{
+			int *const x = r + (i<<8)+(s>>8);
+			if(*x>0)
+				*x -= rx[(i<<16)+s];
+		}
+	}
+	free(rx);
+	strcpy(e->name,"IT-2x");
 }
 
 
@@ -46,7 +138,7 @@ void fit2(struct Estimation *const e)	{
 
 int main(const int argc, const char *argv[])	{
 	const f_estimate farray[] = {
-		fit1,fit2,NULL
+		fit1,fit1x, fit2,fit2x, NULL
 	};
 	const f_estimate *pfun = NULL;
 	int total = 0;
@@ -60,6 +152,8 @@ int main(const int argc, const char *argv[])	{
 		return -2;
 	fseek(fi,0,SEEK_END);
 	total = ftell(fi);
+	if(total<=0)
+		return -3;
 
 	for(pfun=farray; *pfun; ++pfun)	{
 		struct Estimation est = {0,0,""};
