9daae76fb67c35dc2112928c5d5676d6d8e815a6
kvark <kvark@localhost>
2011-12-07 15:49:12 -0500

    a7: moved to unsigned arithmetics

--- bwt/a7/src/archon.h
+++ bwt/a7/src/archon.h
@@ -1,26 +1,26 @@
-typedef	int	suffix;
-typedef unsigned char byte;
-typedef unsigned long dword;
+typedef	unsigned int	suffix;
+typedef unsigned char	byte;
+typedef unsigned long	dword;
 
 
 class Archon	{
 	suffix *const P;
-	int	*const R;
+	unsigned *const R;
 	byte *const str;
 	byte *const bitMask;
-	const int Nmax;
-	int N, baseId;
-	void roll(const int i);
+	const unsigned Nmax;
+	unsigned N, baseId;
+	void roll(const unsigned i);
 
 public:
-	Archon(const int N);
+	Archon(const unsigned N);
 	~Archon();
 	// encoding
-	int en_read(FILE *const fx, int ns);
+	int en_read(FILE *const fx, unsigned ns);
 	int en_compute();
 	int en_write(FILE *const fx);
 	// decoding
-	int de_read(FILE *const fx, int ns);
+	int de_read(FILE *const fx, unsigned ns);
 	int de_compute();
 	int de_write(FILE *const fx);
 };
