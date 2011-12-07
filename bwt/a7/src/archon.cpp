56f6bc435141e958ceeaf686adcf97d288a0597c
kvark <kvark@localhost>
2011-12-07 11:53:17 -0500

    a7: Sentinel handling improved. Works correctly on Unix now.

--- bwt/a7/src/archon.cpp
+++ bwt/a7/src/archon.cpp
@@ -4,6 +4,8 @@
 
 #include "archon.h"
 
+//	Memory requirements:		7.25n
+//	Execution time complexity:	O(n)
 
 //--------------------------------------------------------//
 
@@ -31,10 +33,13 @@ class	Sais	{
 	bool isElbow(const int i) const	{
 		assert(i>=0);
 		const dword dx = *(dword*)( bits+(i>>3) ) >> (i&0x7U);
-		return (dx & 3) == 1;	//has to be 10b
+		return (dx & 3) == 1;	// has to be 10b
 	}
 
+	void checkData();
+
 	void classify()	{
+		checkData();
 		memset( bits, 0, (N>>3)+1 );
 		int prev = data[0]+1;
 		for(int i=0; i!=N; ++i)	{
@@ -54,7 +59,6 @@ class	Sais	{
 		memset( R, 0, K*sizeof(int) );
 		int i,sum;
 		for(i=0; i<N; ++i)	{
-			//assert(data[i]>=0 && data[i]<K);
 			R[data[i]] += 1;
 		}
 		for(R[i=K]=sum=N; i--;)	{
@@ -99,18 +103,23 @@ class	Sais	{
 		// to define the first set of values
 		// and write them into [n1,2*n1)
 		memset( P+n1, -1, (N-n1)*sizeof(suffix) );
-		int prev = -1;
+		suffix prev = 0;
 		for(name=0,i=0; i<n1; ++i)	{
 			const suffix pos = P[i];
 			// we compare the next LMS substring with the previous one
 			for(int d=1; d<=N; ++d)	{
-				assert(prev<0 || data[pos-d] >= data[prev-d]);
-				if(prev<0 || data[pos-d]!=data[prev-d] || isUp(pos-d) != isUp(prev-d))	{
+				assert(!prev || pos<d || data[pos-d] >= data[prev-d]);
+				if(!prev || pos<d || data[pos-d]!=data[prev-d] || isUp(pos-d) != isUp(prev-d))	{
 					// this one is different - remember it
-					//TODO: keep group information here to accelerate farther sorting
+					//todo: keep group information here to accelerate farther sorting
+					assert(pos>=d || (pos==d-1 && prev>pos));
 					++name; prev=pos; break;
-				}else if(d>1 && (isElbow(pos-d) || isElbow(prev-d)))
-					break;
+				}else	{
+					assert( d<=pos && d<=prev );
+					assert( d!=1 || (isElbow(pos-d) && isElbow(prev-d)) );
+					if(d>1 && (isElbow(pos-d) || isElbow(prev-d)))
+						break;
+				}
 			}
 			// write somewhere in the accessible area
 			// it's guaranteed that no two LMS are together
@@ -121,11 +130,12 @@ class	Sais	{
 		}
 		// pack values into the last n1 suffixes
 		int j;
-		for(i=j=N; --i>=n1; )		{
+		for(i=N,j=N; --i>=n1; )		{
 			if(P[i]>=0)
 				P[--j]=P[i];
 		}
-		assert(j+n1 == N);
+		assert(j>0 && j+n1 == N);
+		P[j-1] = name;	// terminator!
 	}
 
 	void solve()	{
@@ -147,6 +157,11 @@ class	Sais	{
 		// LMS number -> actual string number
 		//note: going left to right here!
 		for(i=0,j=0; i<N; ++i)	{
+			//note: this is the only place after recursion
+			// where we need the bit array
+			// moreover, we use it in a sequential order
+			//todo: don't use bit array here,
+			// reducing the memory requirements by N/4
 			if(isElbow(i))	{
 				assert( n1+j<N );
 				s1[j++] = i+1;
@@ -169,10 +184,9 @@ class	Sais	{
 			assert(j>0 && j<=N				&& "Invalid suffix!");
 			assert(data[j-1] <= prev_sym		&& "Not sorted!");
 			int *const pr = RE+data[j-1];
-			suffix *const s = P + --*pr;
+			P[--*pr] = j;
 			assert(pr[0] >= pr[-1]	&& "Stepped twice on the same suffix!");
 			assert(pr[0] >= i		&& "Not sorted properly!");
-			*s = j;
 		}
 		// induce the rest of suffixes
 		induce<0>();
@@ -189,17 +203,23 @@ public:
 	}
 };
 
+template<>	void Sais<byte>::checkData()	{
+	assert(K==0x100);
+}
+template<>	void Sais<suffix>::checkData()	{
+	for(int i=0; i<N; ++i)
+		assert(data[i]>=0 && data[i]<K);
+}
+
 
 //--------------------------------------------------------//
 
 //	INITIALIZATION	//
 
-static const int sTermin = 10;
-
 Archon::Archon(const int Nx)
 : P(new suffix[Nx+1])
 , R(new int[((Nx>>9) ? (Nx>>1) : 256)+1])
-, str(new byte[sTermin+Nx+1])
+, str(new byte[Nx+1])
 , bitMask(new byte[(Nx>>2)+4])
 , Nmax(Nx), N(-1), baseId(-1) {
 	assert(P && R && str && bitMask);
@@ -217,12 +237,12 @@ Archon::~Archon()	{
 
 int Archon::en_read(FILE *const fx, int ns)	{
 	assert(ns>=0 && ns<=Nmax);
-	N = fread( str+sTermin, 1, ns, fx );
+	N = fread( str, 1, ns, fx );
 	return N;
 }
 
 int Archon::en_compute()	{
-	Sais<byte>( str+sTermin, P, bitMask, N, R, 256 );
+	Sais<byte>( str, P, bitMask, N, R, 256 );
 	return 0;
 }
 
@@ -234,7 +254,7 @@ int Archon::en_write(FILE *const fx)	{
 			baseId = i;
 			pos = 0;
 		}
-		fputc( str[sTermin+pos], fx);
+		fputc( str[pos], fx);
 	}
 	fwrite( &baseId, sizeof(int), 1, fx);
 	return 0;
@@ -249,8 +269,7 @@ void Archon::roll(const int i)	{
 }
 
 int Archon::de_read(FILE *const fx, int ns)	{
-	assert(ns>=0 && ns<=Nmax);
-	N = fread( str, 1, ns, fx );
+	en_read(fx,ns);
 	int ok = fread( &baseId, sizeof(int), 1, fx );
 	assert(ok && baseId>=0 && baseId<N);
 	return N;
@@ -258,14 +277,12 @@ int Archon::de_read(FILE *const fx, int ns)	{
 
 int Archon::de_compute()	{
 	int i,k;
-	const byte *const data = str+sTermin;
 	memset( R, 0, 0x100*sizeof(int) );
-	memset( P, -1, (N+1)*sizeof(int) );		//for debug
+	memset( P, -1, (N+1)*sizeof(int) );		// for debug
 	for(i=0; i!=N; ++i)
 		R[str[i]] += 1;
-	for(k=N,i=0x100; i--;)	{
+	for(k=N,i=0x100; i--;)
 		R[i] = (k-=R[i]);
-	}
 	for(i=0; i<baseId; i++)		roll(i);
 	for(i=baseId+1; i<N; i++)	roll(i);
 	roll(baseId);
@@ -276,5 +293,6 @@ int Archon::de_write(FILE *const fx)	{
 	int i,k;
 	for(i=0,k=baseId; i!=N; ++i,k=P[k])
 		putc(str[k],fx);
+	assert( k==baseId );
 	return 0;
 }
