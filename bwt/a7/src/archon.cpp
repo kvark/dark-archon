645841f86d4a3cd58b91b6d8429e8c895f1b2e54
kvark <kvark@localhost>
2012-01-04 09:03:37 -0500

    a7: optimized for Core2 Duo. Data squeezing enabled.

--- bwt/a7/src/archon.cpp
+++ bwt/a7/src/archon.cpp
@@ -29,7 +29,9 @@ class	Constructor	{
 
 	enum	{
 		BIT_LMS		= 31,
-		FLAG_LMS	= 1<<BIT_LMS
+		FLAG_LMS	= 1<<BIT_LMS,
+		BIT_JUMP	= 30,
+		FLAG_JUMP	= 1<<BIT_JUMP,
 	};
 
 	//---------------------------------
@@ -241,7 +243,7 @@ class	Constructor	{
 		*--pr = 1 + (prev<data[1] ? FLAG_LMS:0);
 		i=N; do	{
 			const suffix s = P[--i];
-			if(s>=N-1 || !s)
+			if((unsigned)(s-1) >= N-2)
 				continue;
 			//P[i] = 0;
 			const T cur = data[s];
@@ -272,15 +274,14 @@ class	Constructor	{
 		for(i=0; i!=N; ++i)	{
 			suffix s = P[i];
 			// empty space is supposed to be flagged
-			if((s&FLAG_LMS) || s==N-1 || s==N+N-1)	{
+			if((s&FLAG_LMS) || (s&~FLAG_JUMP)==N-1)	{
 				P[i] = s & ~FLAG_LMS;
 				continue;
 			}
 			assert(s);
 			P[i] = 0;
-			if( s>=N )	{
-				s-=N; ++d;
-			}
+			d += s>>BIT_JUMP;
+			s &= ~FLAG_JUMP;
 			const T cur = data[s];
 			assert( data[s-1] <= cur );
 			if(cur != prev)	{
@@ -291,31 +292,33 @@ class	Constructor	{
 			unsigned t = (cur<<1) + (data[s+1]<cur);
 			suffix q = s+1;
 			if(D[t] != d)	{
-				q+=N; D[t]=d;
+				q |= FLAG_JUMP;
+				D[t] = d;
 			}//todo: can be optimized
-			*pr++ = q + (t<<BIT_LMS);
+			*pr++ = q | (t<<BIT_LMS);
 		}
 		//reverse flags order
 		i=N; do	{
 			const suffix s = P[--i];
-			if(s && s<N)	{
-				P[i] += N;
-				while( assert(i>0), P[--i]<N );
-				P[i] -= N;
+			// exclude 0 and N+
+			if((unsigned)(s-1) < N-1U)	{
+				P[i] |= FLAG_JUMP;
+				while( assert(i>0), !(P[--i]&FLAG_JUMP) );
+				P[i] ^= FLAG_JUMP;
 			}
 		}while(i);
 		//right2left
 		buckets();
 		pr = P + RE[prev=data[0]];
-		*--pr = 1 + N + (prev<data[1] ? FLAG_LMS:0);
+		*--pr = 1 | FLAG_JUMP | (prev<data[1] ? FLAG_LMS:0);
 		i=N; ++d; do	{
 			suffix s = P[--i];
-			if((s&FLAG_LMS) || s==N-1 || s==N+N-1 || !s)
+			// exclude N-1 and 0 and LMS flags
+			if((unsigned)((s&~FLAG_JUMP)-1) >= N-2U)
 				continue;
 			//P[i] = 0;
-			if( s>=N )	{
-				s-=N; ++d;
-			}
+			d += s>>BIT_JUMP;
+			s &= ~FLAG_JUMP;
 			const T cur = data[s];
 			assert( data[s-1] >= cur );
 			if(cur != prev)	{
@@ -326,9 +329,10 @@ class	Constructor	{
 			unsigned t = (cur<<1) + (data[s+1]>cur);
 			suffix q = s+1;
 			if(D[t] != d)	{
-				q+=N; D[t]=d;
+				q |= FLAG_JUMP;
+				D[t] = d;
 			}//todo: can be optimized
-			*--pr = q + (t<<BIT_LMS);
+			*--pr = q | (t<<BIT_LMS);
 		}while(i);
 	}
 
@@ -346,7 +350,7 @@ class	Constructor	{
 			P[i] = s ^ FLAG_LMS;
 			if(s & FLAG_LMS)
 				continue;
-			assert(s && s!=N);
+			assert(s && s<N);
 			const T cur = data[s];
 			assert( data[s-1] <= cur );
 			if(cur != prev)	{
@@ -355,15 +359,15 @@ class	Constructor	{
 			}
 			assert( pr>P+i && pr<P+RE[cur] );
 			const suffix q = s+1;
-			*pr++ = q + (q==N || cur>data[q] ? FLAG_LMS:0);
+			*pr++ = q | (q==N || cur>data[q] ? FLAG_LMS:0);
 		}
 		//right2left
 		buckets();
 		pr = P + RE[prev=data[0]];
-		*--pr = 1 + (prev<data[1] ? FLAG_LMS:0);
+		*--pr = 1 | (prev<data[1] ? FLAG_LMS:0);
 		i=N; do	{
 			const suffix s = P[--i];
-			if((s&FLAG_LMS) || s==N)	{
+			if(s>=N)	{
 				P[i] = s & ~FLAG_LMS;
 				continue;
 			}
@@ -376,7 +380,7 @@ class	Constructor	{
 			}
 			assert( pr>P+R[cur] && pr<=P+i );
 			const suffix q = s+1;
-			*--pr = q + (q!=N && cur<data[q] ? FLAG_LMS:0);
+			*--pr = q | (q!=N && cur<data[q] ? FLAG_LMS:0);
 		}while(i);
 	}
 
@@ -419,7 +423,7 @@ class	Constructor	{
 		for(R2[-1]=0;;)	{
 			if(RE[i]!=top)	{
 				assert( RE[i]<top && P[RE[i]] );
-				P[RE[i]] += N;
+				P[RE[i]] |= FLAG_JUMP;
 			}
 			if(!i)
 				break;
@@ -434,8 +438,8 @@ class	Constructor	{
 			if(!(s&FLAG_LMS))
 				continue;
 			s &= ~FLAG_LMS;
-			if(s>=N)	{
-				if(s==N+N)
+			if(s & FLAG_JUMP)	{
+				if(s==(N|FLAG_JUMP))
 					continue;
 				++name;
 			}
@@ -450,8 +454,9 @@ class	Constructor	{
 			top = name+1;
 			i=n1; do	{
 				suffix suf = P[--i];
-				if(suf>=N)	{
-					suf-=N; --top;
+				if(suf & FLAG_JUMP)	{
+					suf ^= FLAG_JUMP;
+					--top;
 				}
 				s1[suf>>1] = top;
 			}while(i);
@@ -459,8 +464,7 @@ class	Constructor	{
 		}else	{
 			d1 = n1;
 			for(i=0; i!=n1; ++i)	{
-				if(P[i]>N)
-					P[i] -= N;
+				P[i] &= ~FLAG_JUMP;
 			}
 			return false;
 		}
